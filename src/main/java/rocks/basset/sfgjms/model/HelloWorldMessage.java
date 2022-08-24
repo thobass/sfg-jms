7ef427684dd62c617acdc1f7f9352d178588f3a2
BASSET, Thomas <thomas.basset@cgi.com>
2022-08-24 12:01:18 +0200

    Java message object

---
+++ src/main/java/rocks/basset/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package rocks.basset.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    private static final long serialVersionUID = 483934739383542197L;
+
+    private UUID id;
+    private String message;
+}
