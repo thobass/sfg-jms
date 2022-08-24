fd23ef0212031e0cdc3b35cd1364474b6f416d30
BASSET, Thomas <thomas.basset@cgi.com>
2022-08-24 12:25:15 +0200

    Sending JMS Message

---
+++ src/main/java/rocks/basset/sfgjms/sender/HelloSender.java
@@ -0,0 +1,31 @@
+package rocks.basset.sfgjms.sender;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import rocks.basset.sfgjms.config.JmsConfig;
+import rocks.basset.sfgjms.model.HelloWorldMessage;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage(){
+        System.out.println("I'm sending a message");
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello World!")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+        System.out.println("Message sent");
+    }
+}
