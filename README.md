7d29e212bc163958267ea23b505f44a53d705766
BASSET, Thomas <thomas.basset@cgi.com>
2022-08-24 11:55:36 +0200

    Projet setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# JMS Demo project
---
+++ src/test/java/rocks/basset/sfgjms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.basset.sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
