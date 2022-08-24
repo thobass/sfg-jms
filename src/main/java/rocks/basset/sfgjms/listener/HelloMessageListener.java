3a32c56c20478f9cf78588a66b93fde6ea3a1ea6
BASSET, Thomas <thomas.basset@cgi.com>
2022-08-24 12:59:05 +0200

    Send and Receiving JMS Message

--- src/main/java/rocks/basset/sfgjms/listener/HelloMessageListener.java
+++ src/main/java/rocks/basset/sfgjms/listener/HelloMessageListener.java
@@ -1,7 +1,8 @@
 package rocks.basset.sfgjms.listener;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.messaging.Message;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -9,14 +10,36 @@ import org.springframework.stereotype.Component;
 import rocks.basset.sfgjms.config.JmsConfig;
 import rocks.basset.sfgjms.model.HelloWorldMessage;
 
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import java.util.UUID;
+
 @Component
+@RequiredArgsConstructor
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
                        @Headers MessageHeaders headers,
                        Message message){
-        System.out.println("I Got a message !!!!!");
-        System.out.println(helloWorldMessage);
+//        System.out.println("I Got a message !!!!!");
+//        System.out.println(helloWorldMessage);
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers,
+                       Message message) throws JMSException {
+
+        HelloWorldMessage messageResponse = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("World")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(),messageResponse);
     }
 }
--- src/main/java/rocks/basset/sfgjms/sender/HelloSender.java
+++ src/main/java/rocks/basset/sfgjms/sender/HelloSender.java
@@ -1,12 +1,18 @@
 package rocks.basset.sfgjms.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import rocks.basset.sfgjms.config.JmsConfig;
 import rocks.basset.sfgjms.model.HelloWorldMessage;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -14,11 +20,10 @@ import java.util.UUID;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage(){
-        System.out.println("I'm sending a message");
-
         HelloWorldMessage message = HelloWorldMessage
                 .builder()
                 .id(UUID.randomUUID())
@@ -26,6 +31,34 @@ public class HelloSender {
                 .build();
 
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
-        System.out.println("Message sent");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendReceivedMessage() throws JMSException {
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello")
+                .build();
+
+        Message receivedMessage = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                Message helloMessage = null;
+                try {
+                    helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                    helloMessage.setStringProperty("_type", "rocks.basset.sfgjms.model.HelloWorldMessage");
+
+                    System.out.println("Sending hello");
+
+                    return helloMessage;
+                } catch (JsonProcessingException e) {
+                    throw new JMSException("boom");
+                }
+            }
+        });
+
+        System.out.println(receivedMessage.getBody(String.class));
     }
 }
